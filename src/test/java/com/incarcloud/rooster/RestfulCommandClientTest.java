9015e94f7e4a0d23327bfa31689c4b07ea78af45
aaric <vipaaric@gmail.com>
2017-11-28 15:47:34 +0800

    rm rooster-shared

--- src/test/java/com/incarcloud/rooster/RestfulCommandClientTest.java
+++ src/test/java/com/incarcloud/rooster/RestfulCommandClientTest.java
@@ -6,6 +6,7 @@ import com.incarcloud.rooster.gather.cmd.CommandType;
 import com.incarcloud.rooster.gather.cmd.RespContent;
 import com.incarcloud.rooster.gather.cmd.client.CommandClient;
 import com.incarcloud.rooster.remotecmd.client.ZookeeperRestfulCommandClient;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -14,11 +15,13 @@ import org.junit.Test;
  * @date 2017/7/20 11:07
  */
 public class RestfulCommandClientTest {
-//    @Test
+
+    @Test
+    @Ignore
     public void testSend(){
         CommandClient client = new ZookeeperRestfulCommandClient("127.0.0.1:2181");
         try {
-            RespContent resp =  client.sendCommand("1A1JC5444R7252367", CommandType.FLASH_LIGHTS_ON);
+            RespContent resp =  client.sendCommand("1A1JC5444R7252367", CommandType.FLASH_LIGHTS_ON, null);
 
             System.out.println(resp);
         }catch (Exception e){
