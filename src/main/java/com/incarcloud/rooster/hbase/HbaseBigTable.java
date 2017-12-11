e85c8ab7d57c8dc42ebbafac17e32261a1df5a43
aaric <vipaaric@gmail.com>
2017-12-11 17:38:54 +0800

    优化代码结构，解决单元测试问题

--- src/main/java/com/incarcloud/rooster/hbase/HbaseBigTable.java
+++ src/main/java/com/incarcloud/rooster/hbase/HbaseBigTable.java
@@ -5,14 +5,14 @@ package com.incarcloud.rooster.hbase;/**
 import com.incarcloud.rooster.bigtable.IBigTable;
 import com.incarcloud.rooster.datapack.DataPackObject;
 import com.incarcloud.rooster.util.DataPackObjectUtils;
+import com.incarcloud.rooster.util.HBaseUtil;
 import com.incarcloud.rooster.util.RowKeyUtil;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.client.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,27 +27,34 @@ import java.util.Properties;
  * @date 2017/7/10 18:04
  */
 public class HbaseBigTable implements IBigTable {
-    private static Logger s_logger = LoggerFactory.getLogger(HbaseBigTable.class);
+
+    /**
+     * Logger
+     */
+    private static Logger logger = LoggerFactory.getLogger(HbaseBigTable.class);
+
     /**
      * 保存vin码的表
      */
-    private static final String VIN_TABLE = "vehicle";
+    private static final String TABLE_NAME_VEHICLE = "vehicle";
+
     /**
      * 二级索引表
      */
-    private static final String SECOND_INDEX_TABLE = "second_index";
+    private static final String TABLE_NAME_SECOND_INDEX = "second_index";
 
     /**
      * 列族
      */
-    private static final String COLUMN_FAMILY = "base";
+    private static final String COLUMN_FAMILY_NAME = "base";
+
     /**
      * 数据列
      */
-    private static final String COLUMN_DATA = "data";
+    private static final String COLUMN_NAME_DATA = "data";
 
     /**
-     * hbase 连接，重量级且线程安全，建议单例
+     * HBase连接，重量级且线程安全，建议单例
      */
     private Connection connection;
 
@@ -55,21 +62,47 @@ public class HbaseBigTable implements IBigTable {
         if (!validate(props)) {
             throw new IllegalArgumentException();
         }
-
         if (null == System.getenv("HADOOP_HOME")) {
-            throw new IllegalArgumentException("environment variable  HADOOP_HOME is null!!");
+            throw new IllegalArgumentException("environment variable 'HADOOP_HOME' is null!");
         }
 
+        /* 创建HBase连接 */
         Configuration configuration = HBaseConfiguration.create();
         configuration.set("hbase.zookeeper.property.clientPort", props.getProperty("hbase.zookeeper.property.clientPort"));
         configuration.set("hbase.zookeeper.quorum", props.getProperty("hbase.zookeeper.quorum"));
         configuration.set("hbase.master", props.getProperty("hbase.master"));
-
         connection = ConnectionFactory.createConnection(configuration);
+
+        /* 创建数据表 */
+        // admin
+        Admin admin = connection.getAdmin();
+
+        // create vehicle
+        HTableDescriptor desc;
+        TableName tableName = TableName.valueOf(TABLE_NAME_VEHICLE);
+        if (!admin.tableExists(tableName)) {
+            desc = new HTableDescriptor(tableName);
+            admin.createTable(desc.addFamily(new HColumnDescriptor(COLUMN_FAMILY_NAME)));
+        }
+
+        // create second_index
+        tableName = TableName.valueOf(TABLE_NAME_SECOND_INDEX);
+        if (!admin.tableExists(tableName)) {
+            desc = new HTableDescriptor(tableName);
+            admin.createTable(desc.addFamily(new HColumnDescriptor(COLUMN_FAMILY_NAME)));
+        }
+
+        // create telemetry
+        tableName = TableName.valueOf(DataPackObjectUtils.getTableName("default"));
+        if (!admin.tableExists(tableName)) {
+            desc = new HTableDescriptor(tableName);
+            admin.createTable(desc.addFamily(new HColumnDescriptor(COLUMN_FAMILY_NAME)));
+        }
     }
 
     /**
      * 验证参数
+     *
      * @param props
      * @return
      */
@@ -77,59 +110,53 @@ public class HbaseBigTable implements IBigTable {
         if (null == props) {
             return false;
         }
-
         if (null == props.get("hbase.zookeeper.quorum")) {
             return false;
         }
-
-
         if (null == props.get("hbase.master")) {
             return false;
         }
-
         if (null == props.get("hbase.zookeeper.property.clientPort")) {
             return false;
         }
-
         return true;
     }
 
     @Override
     public void saveDataPackObject(String rowKey, DataPackObject data, Date recieveTime) throws Exception {
+        /* 保存二级索引 */
+        Table indexTable = connection.getTable(TableName.valueOf(TABLE_NAME_SECOND_INDEX));
+        String secondIndexRowKey = RowKeyUtil.makeDetectionTimeIndexRowKey(DataPackObjectUtils.convertDetectionDateToString(data.getDetectionTime()), data.getVin(), DataPackObjectUtils.getDataType(data));
 
-        //保存二级索引
-        Table indexTable = connection.getTable(TableName.valueOf(SECOND_INDEX_TABLE));
-        String secondIndexRowKey = RowKeyUtil.makeDetectionTimeIndexRowKey(DataPackObjectUtils.convertDetectionDateToString(data.getDetectionTime()),
-                data.getVin(), DataPackObjectUtils.getDataType(data));
-        Put indexPut = new Put(secondIndexRowKey.getBytes());// 一个PUT代表一行数据，再NEW一个PUT表示第二行数据,每行一个唯一的ROWKEY，此处rowkey为put构造方法中传入的值
-        indexPut.addColumn(COLUMN_FAMILY.getBytes("UTF-8"), COLUMN_DATA.getBytes("UTF-8"),
-                rowKey.getBytes("UTF-8"));
+        // 一个PUT代表一行数据，再NEW一个PUT表示第二行数据,每行一个唯一的ROWKEY，此处rowkey为put构造方法中传入的值
+        Put indexPut = new Put(secondIndexRowKey.getBytes());
+        indexPut.addColumn(HBaseUtil.valueOf(COLUMN_FAMILY_NAME), HBaseUtil.valueOf(COLUMN_NAME_DATA), HBaseUtil.valueOf(rowKey));
         indexTable.put(indexPut);
 
+        /* 保存DataPack数据 */
+        // Table对象线程不安全
+        Table dataTable = connection.getTable(TableName.valueOf(DataPackObjectUtils.getTableName("default")));
 
-        //保存数据
-        Table dataTable = connection.getTable(TableName.valueOf(DataPackObjectUtils.getTableName(DataPackObjectUtils.getDataType(data))));//tabel对象线程不安全
-        Put dataPut = new Put(rowKey.getBytes());// 一个PUT代表一行数据，再NEW一个PUT表示第二行数据,每行一个唯一的ROWKEY，此处rowkey为put构造方法中传入的值
-        dataPut.addColumn(COLUMN_FAMILY.getBytes("UTF-8"), COLUMN_DATA.getBytes("UTF-8"),
-                DataPackObjectUtils.toJson(data).getBytes("UTF-8"));
+        // 一个PUT代表一行数据，再NEW一个PUT表示第二行数据,每行一个唯一的ROWKEY，此处rowkey为put构造方法中传入的值
+        Put dataPut = new Put(rowKey.getBytes());
+        dataPut.addColumn(HBaseUtil.valueOf(COLUMN_FAMILY_NAME), HBaseUtil.valueOf(COLUMN_NAME_DATA), HBaseUtil.valueOf(DataPackObjectUtils.toJson(data)));
         dataTable.put(dataPut);
 
         // TODO recieveTime 接收时间
-
-        s_logger.debug("saveDataPackObject success.  "+rowKey);
+        logger.debug("Save data pack object for vin({}) success!", rowKey);
     }
 
     @Override
     public void saveVin(String vin) throws Exception {
+        // Table对象线程不安全
+        Table dataTable = connection.getTable(TableName.valueOf(TABLE_NAME_VEHICLE));
 
-        Table dataTable = connection.getTable(TableName.valueOf(VIN_TABLE));//tabel对象线程不安全
-        Put dataPut = new Put(vin.getBytes());// 一个PUT代表一行数据，再NEW一个PUT表示第二行数据,每行一个唯一的ROWKEY，此处rowkey为put构造方法中传入的值
-        dataPut.addColumn(COLUMN_FAMILY.getBytes("UTF-8"), COLUMN_DATA.getBytes("UTF-8"),
-                vin.getBytes("UTF-8"));
+        // 一个PUT代表一行数据，再NEW一个PUT表示第二行数据，每行一个唯一的ROWKEY，此处rowkey为put构造方法中传入的值
+        Put dataPut = new Put(vin.getBytes());
+        dataPut.addColumn(HBaseUtil.valueOf(COLUMN_FAMILY_NAME), HBaseUtil.valueOf(COLUMN_NAME_DATA), HBaseUtil.valueOf(vin));
         dataTable.put(dataPut);
 
-        s_logger.debug("save  vin  success.  "+vin);
-
+        logger.debug("Save vin({}) success.", vin);
     }
 
     @Override
@@ -150,7 +177,7 @@ public class HbaseBigTable implements IBigTable {
             try {
                 connection.close();
             } catch (IOException e) {
-                s_logger.error(e.getMessage());
+                logger.error(e.getMessage());
             }
         }
 
